c070d3c0fb3f529cad72f92b7e57906c52fc35bb
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-04-29 13:18:20 +0300

    commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React,{useState}from "react";
 
 function Item({ name, category }) {
+  const [addInCart, setCart] = useState() 
+  function handleCart()  {
+    setCart(!addInCart) 
+  }
+  const liClassName = addInCart ? "in-cart" : "";
+ const buttonText = addInCart ? "Remove From Cart" : "Add to Cart";
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{buttonText}</button>
     </li>
   );
 }
